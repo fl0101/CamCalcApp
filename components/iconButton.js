1cebfeb67758f4f18ab5f87a5bf79b8f6d781453
Flaviane Silva <fl0101@protonmail.com>
2024-07-12 22:49:03 -0300

    Button Component: Created 'delete' button component

---
+++ components/iconButton.js
@@ -0,0 +1,18 @@
+import { Pressable, StyleSheet, } from 'react-native';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+
+export default function IconButton({ icon, onPress }) {
+    return (
+        <Pressable style={styles.iconButton} onPress={onPress}>
+            <MaterialIcons name={icon} size={34} color="#fff" />
+        </Pressable>
+    );
+}
+
+const styles = StyleSheet.create({
+    iconButton: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 5,
+    },
+});
